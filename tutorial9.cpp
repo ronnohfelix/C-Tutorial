e77e66afd15cfe37c805434f45f30e9a8dd16dec
Felix Kipchirchir Ronoh <felixronoh258@gmail.com>
2024-01-29 12:03:33 +0300

    arrays

---
+++ tutorial9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//arrays
+
+int main() {
+    int luckyNums[] = {4, 8, 15, 16, 23, 42}; // array of integers
+    cout << luckyNums[0] << endl;
+    cout << sizeof(luckyNums) << endl; // size of array in bytes
+    string friends[] = {"Kevin", "Karen", "Jim"}; // array of strings
+    cout << friends[1];
+    return 0;
+}
