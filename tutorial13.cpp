8ffdd46e41886820c3c29cc50a52eca067dd3838
Felix Kipchirchir Ronoh <felixronoh258@gmail.com>
2024-02-03 20:32:36 +0300

    strings

---
+++ tutorial13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// strings
+
+int main() {
+    string phrase = "Giraffe Academy";
+    cout << phrase.length() << endl;
+    cout << phrase[0] << endl;
+    phrase[0] = 'B';
+    cout << phrase << endl;
+    cout << phrase.find("Academy", 0) << endl;
+    cout << phrase.substr(8, 3) << endl;
+
+    return 0;
+}
